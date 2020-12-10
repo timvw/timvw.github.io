47fc81680ff078f223a5f00157baefc9ccda6ef5
Tim Van Wassenhove <tim@timvw.be>
2020-12-10 07:39:49 +0100

    update gem and run

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 rm $PWD/Gemfile.lock
 #docker run -i -t --rm -v "$PWD:/srv/jekyll" -v "/tmp/jekyll:/usr/local/bundle" -p "4000:4000" jekyll/jekyll $@
 #docker run -i -t --rm -v "$PWD:/srv/jekyll" -v "/tmp/jekyll:/usr/local/bundle" -p "4000:4000" jekyll/jekyll bundle install
-docker run -i -t --rm -v "$PWD:/srv/jekyll" -v "/tmp/jekyll:/usr/local/bundle" -p "4000:4000" jekyll/jekyll jekyll build
-docker run -i -t --rm -v "$PWD:/srv/jekyll" -v "/tmp/jekyll:/usr/local/bundle" -p "4000:4000" jekyll/jekyll jekyll serve --watch --force_polling --incremental -H 0.0.0.0 -P 4000
+#docker run -i -t --rm -v "$PWD:/srv/jekyll" -v "/tmp/jekyll:/usr/local/bundle" -p "4000:4000" jekyll/jekyll jekyll build
+#docker run -i -t --rm -v "$PWD:/srv/jekyll" -v "/tmp/jekyll:/usr/local/bundle" -p "4000:4000" jekyll/jekyll jekyll serve --watch --force_polling --incremental -H 0.0.0.0 -P 4000
+
+docker run -i -t --rm -v "$PWD:/srv/jekyll" -v "/tmp/jekyll:/usr/local/bundle" -p "4000:4000" jekyll/jekyll:pages jekyll serve --watch --force_polling --incremental -H 0.0.0.0 -P 4000
